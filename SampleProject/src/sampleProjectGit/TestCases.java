4a8b8a1ec8cbfa5f2e055bd215854da33af7d280
user <user@user-PC>
2020-12-03 12:31:12 +0530

    Commit 4

--- SampleProject/src/sampleProjectGit/TestCases.java
+++ SampleProject/src/sampleProjectGit/TestCases.java
@@ -1,5 +1,6 @@
 package sampleProjectGit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -9,12 +10,24 @@ public class TestCases {
 	public void LoginTest()
 	{
 		System.out.println("Welcome");
-		System.out.println("Welcome 2");
+		System.out.println("Test Case Starts");
 		System.setProperty("webdriver.chrome.driver", "C:/Users/user/git/SampleProject/SampleProject/Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.orangehrm.com");
 		driver.manage().window().maximize();
-		System.out.println("Welcome 2");
+		driver.findElement(By.linkText("Services")).click();
+		System.out.println("Clicked on Services Link");
+		driver.findElement(By.linkText("Features")).click();
+		System.out.println("Clicked on Features Link");
+		driver.findElement(By.linkText("Success Stories")).click();
+		System.out.println("Clicked on Success Stories Link");
+		driver.findElement(By.linkText("Partners")).click();
+		System.out.println("Clicked on Partners Link");
+		driver.findElement(By.linkText("Blog")).click();
+		System.out.println("Clicked on Blog Link");
+		driver.findElement(By.linkText("Resources")).click();
+		System.out.println("Clicked on Resources Link");
+		System.out.println("Test Case Ends");
 		
 	}
 
