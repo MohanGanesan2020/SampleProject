3fa461af6483082be4f7dc3927db90f7bd4a1073
user <user@user-PC>
2020-12-03 17:05:16 +0530

    Commit 6

--- SampleProject/src/testCases/testCases.java
+++ SampleProject/src/testCases/testCases.java
@@ -1,4 +1,4 @@
-package sampleProjectGit;
+package testCases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 public class testCases {
 	@Test
-	public void LoginTest()
+	public void LoginTest() throws InterruptedException
 	{
 		System.out.println("Welcome");
 		System.out.println("Test Case Starts");
@@ -15,19 +15,35 @@ public class testCases {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.orangehrm.com");
 		driver.manage().window().maximize();
+		
+		//Service Page 
 		driver.findElement(By.linkText("Services")).click();
 		System.out.println("Clicked on Services Link");
+		Thread.sleep(30000);
+		driver.findElement(By.linkText("Premier Open Source Support")).click();
+		
+		
+		/*//Features Page
 		driver.findElement(By.linkText("Features")).click();
 		System.out.println("Clicked on Features Link");
+		
+		
+		//Success Stories Page
 		driver.findElement(By.linkText("Success Stories")).click();
 		System.out.println("Clicked on Success Stories Link");
+		
+		//Partners Page
 		driver.findElement(By.linkText("Partners")).click();
 		System.out.println("Clicked on Partners Link");
+		
+		//Blog Page
 		driver.findElement(By.linkText("Blog")).click();
 		System.out.println("Clicked on Blog Link");
+		
+		//Resources Page
 		driver.findElement(By.linkText("Resources")).click();
 		System.out.println("Clicked on Services Link");
-		System.out.println("Test Case Ends");
+		System.out.println("Test Case Ends");*/
 
 	}
 
