9175a36ee308625316c06481360d0cce5518ae22
user <user@user-PC>
2020-12-03 19:09:17 +0530

    Removed featuresTestCases from testCases Package

--- SampleProject/src/testCases/servicesTestCases.java
+++ SampleProject/src/testCases/servicesTestCases.java
@@ -17,37 +17,18 @@ public class servicesTestCases {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.orangehrm.com");
 		driver.manage().window().maximize();
-		
+
 		//Service Page 
-		/*driver.findElement(By.linkText("Services")).click();
-		System.out.println("Clicked on Services Link");
-		Thread.sleep(10000);*/
 		ServicesPageObjects servicesPageObjects = new ServicesPageObjects();
 		servicesPageObjects.PreOpenSrcSupp(driver);
 		//System.out.println("Clicked on Premier Open Source Support Link");
-		
-				
-		/*//Features Page
-				driver.findElement(By.linkText("Features")).click();
-				System.out.println("Clicked on Features Link");
-				driver.findElement(By.linkText("Learn More")).click();*/
-		
-		/*//Success Stories Page
-		driver.findElement(By.linkText("Success Stories")).click();
-		System.out.println("Clicked on Success Stories Link");
-		
-		//Partners Page
-		driver.findElement(By.linkText("Partners")).click();
-		System.out.println("Clicked on Partners Link");
-		
-		//Blog Page
-		driver.findElement(By.linkText("Blog")).click();
-		System.out.println("Clicked on Blog Link");
-		
-		//Resources Page
-		driver.findElement(By.linkText("Resources")).click();
-		System.out.println("Clicked on Services Link");
-		System.out.println("Test Case Ends");*/
+
+
+		//Features Page
+		driver.findElement(By.linkText("Features")).click();
+		System.out.println("Clicked on Features Link");
+		driver.findElement(By.linkText("Learn More")).click();
+
 
 	}
 
